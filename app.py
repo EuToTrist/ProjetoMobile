9e2539e56573af3ee7e62e4a336d73bfb04f5770
Kayllane Justino <kayllanejmelo@gmail.com>
2023-12-12 10:26:16 -0300

    Adicionando doenças e sintomas

--- app.py
+++ app.py
@@ -4,8 +4,50 @@ doenças = {
     },
     'catapora': {
         'sintomas': ['manchas vermelhas', 'coceira', 'febre']
+    
+    },
+    'bronquite': {
+        'sintomas': ['tosse', 'producao de muco', 'cansaco', 'chiado no peito']
+    },
+    'diabetes': {
+        'sintomas': ['sede excessiva', 'fome intensa', 'perda de peso', 'frequencia urinaria aumentada']
+    },
+    'hipertensao': {
+        'sintomas': ['dores de cabeca', 'tonturas', 'visao embaçada', 'falta de ar']
+    },
+    'asma': {
+        'sintomas': ['dificuldade de respirar', 'tosse', 'chiado', 'opressao no peito']
+    },
+    'dengue': {
+        'sintomas': ['febre alta', 'dores musculares', 'dor atras dos olhos', 'manchas vermelhas na pele']
+    },
+    'zika': {
+        'sintomas': ['febre baixa', 'conjuntivite', 'dor nas articulacoes', 'manchas vermelhas no corpo']
+    },
+    'chikungunya': {
+        'sintomas': ['febre', 'dor nas articulacoes', 'edema', 'rash cutaneo']
+    },
+    'anemia': {
+        'sintomas': ['fadiga', 'palidez', 'falta de ar', 'tontura']
+    },
+    'artrite': {
+        'sintomas': ['dor nas articulacoes', 'inchaço', 'vermelhidao', 'diminuicao da amplitude de movimento']
+    },
+    'conjuntivite': {
+        'sintomas': ['vermelhidao', 'coceira nos olhos', 'secrecao ocular', 'sensacao de areia nos olhos']
+    },
+    'gastrite': {
+        'sintomas': ['dor ou ardor no estomago', 'nausea', 'vomito', 'sensacao de estomago cheio']
+    },
+    'hepatite': {
+        'sintomas': ['ictericia', 'urina escura', 'fadiga extrema', 'nausea']
+    },
+    'malaria': {
+        'sintomas': ['febre', 'calafrios', 'sudorese', 'dores de cabeca']
+    },
+    'meningite': {
+        'sintomas': ['dor de cabeca severa', 'febre alta', 'rigidez no pescoco', 'sensibilidade a luz']
     }
-    # Adicione mais doenças conforme necessário
 }
 
 from flask import Flask, jsonify  # Adicione jsonify aqui
@@ -13,7 +55,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, supports_credentials=True, resources={r"/*: {"origins": "*"}"})
+CORS(app, supports_credentials=True, resources={r"/": {"origins": ""}})
 
 @app.route('/')
 def hello_world():
@@ -23,5 +65,5 @@ def hello_world():
 def listar_doencas():
     return jsonify(doenças)  # Isso irá funcionar agora que você importou jsonify
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     app.run()
