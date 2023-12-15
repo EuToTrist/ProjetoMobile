6c7efdaab9f5c6cef419a12b5984e96edee02768
Die Tastes <waltertelesmn@gmail.com>
2023-12-15 12:16:04 -0300

    Seguindo o exemplo do professor, criação do DoençaActivity

---
+++ app/src/main/java/com/example/clinicease/DoencaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.clinicease
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.clinicease.R
+
+class DoencaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doenca)
+    }
+}
