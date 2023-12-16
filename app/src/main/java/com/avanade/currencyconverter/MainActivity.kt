2cbf2d24561bcd39f5a5e454f913e5353ca5d509
EuToTrist <arthurwillyams.dev@gmail.com>
2023-12-16 06:55:17 -0300

    first commit
    Signed-off-by: EuToTrist <arthurwillyams.dev@gmail.com>

--- app/src/main/java/com/avanade/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/avanade/currencyconverter/MainActivity.kt
@@ -2,41 +2,56 @@ package com.avanade.currencyconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.lifecycle.GeneratedAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.avanade.currencyconverter.api.Endpoint
 import com.avanade.currencyconverter.util.NetworkUtils
 import com.google.gson.JsonObject
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CardAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recyclerView = findViewById(R.id.recycler_view) // Corrigido o nome da variável
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
+        getCurrencies()
     }
 
-    fun getSintomasDeDoencas(doenca: String) {
+    private fun getCurrencies() {
         val retrofitClient = NetworkUtils.getRetrofitInstance("https://clinicease-d5bq34i2.b4a.run/")
         val endpoint = retrofitClient.create(Endpoint::class.java)
 
-        endpoint.getSintomas(doenca).enqueue(object  : retrofit2.Callback<JsonObject> {
+        endpoint.getCurrencies().enqueue(object : Callback<JsonObject> {
             override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
                 if (response.isSuccessful) {
-                    val sintomasObejct = response.body()
+                    val dataList = mutableListOf<String>()
+                    val sintomasObject = response.body()
 
-                    sintomasObejct?.getAsJsonArray("sintomas")?.forEach {
-                        println(it.asString)
+                    sintomasObject?.keySet()?.forEach { doenca ->
+                        dataList.add(doenca)
                     }
-                }else {
-                    println("deu erro")
+
+                    adapter = CardAdapter(dataList)
+                    recyclerView.adapter = adapter
+                } else {
+                    println("erro")
                 }
             }
 
             override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-                println("Erro na requisição: ${t.message}")
+                println("erro na requisiçao")
             }
         })
     }
-
 }
